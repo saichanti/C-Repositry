4c7305b334ebbf30ce5731b2ce2f37d6d757d5d6
saichanti <mosurisaikumar@gmail.com>
2022-05-17 18:04:20 +0530

    c# classes

---
+++ DemoProject/DemoProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            smallClass samllClassObj = new smallClass();
+            samllClassObj.GetDetails();
+
+            string firstName = "sai ";
+            string lastName = "kumar";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);
+
+            int x = 2;
+            int y = 9;
+            Console.WriteLine(x + y);
+
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+
+            smallClass samllClassObj1 = new smallClass();
+            samllClassObj1.Details();
+
+           /* Console.WriteLine("Enter username:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is: " + name);
+
+            Console.WriteLine("Enter your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Your age is: " + age);*/
+
+            int sum1 = 100;
+            int sum2 = 250;   
+            int sum3 = sum2 + sum2;  
+            Console.WriteLine(sum3);
+            Console.WriteLine(sum1*sum2);
+            Console.WriteLine(sum2-sum1);
+        }
+
+        }
+    }
+
---
+++ DemoProject/DemoProject/smallClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    internal class smallClass
+
+    {
+        public string Name;
+
+        public void GetDetails()
+        {
+            string message = "iam from class1!";
+
+            Console.WriteLine(message);
+
+            int myNum = 667400000;
+            Console.WriteLine(myNum);
+        }
+        public void Details()
+        {
+            string message =  "class1 is too good";
+
+            Console.WriteLine(message);
+
+            int x = 87, y = 32, z = 65;
+            Console.WriteLine(x + y + z);
+        }
+
+    }
+}
