b20b51bc2b445a4ef85d9517df99304ef5aba87f
saichanti <mosurisaikumar@gmail.com>
2022-06-21 14:51:54 +0530

    EFAssignment

---
+++ EntityFrameWorkAssignment/EntityFrameWorkAssignment/DemoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameWorkAssignment.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkAssignment
+{
+    public class DemoDbContext : DbContext
+
+
+    {
+        public DbSet<ItemMaster> ItemMasters { get; set; }
+        public DbSet<CustomerMaster> CustomerMasters { get; set; }
+
+
+
+        public DemoDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-70LPGGG\MSSQLSERVER01;Database=Study;Trusted_Connection=True;");
+
+        }
+
+    }
+}
---
+++ EntityFrameWorkAssignment/EntityFrameWorkAssignment/Entities/CustomerMaster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkAssignment.Entities
+{
+    
+        public class CustomerMaster
+        {
+
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+            [Key]
+
+            public string? Customer_FName { get; set; }
+
+            public string? Customer_LName { get; set; }
+
+            public int Customer_Phone { get; set; }
+
+            [Required]
+            public string? Customer_Email { get; set; }
+
+
+        }
+    }
+
